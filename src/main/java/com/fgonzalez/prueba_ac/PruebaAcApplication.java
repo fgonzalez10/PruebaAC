effb0adf09dac168b8dc0241ddea04f8dbb3d953
Francisco Gonzalez <j.francisco.gonzalez09@gmail.com>
2025-11-27 12:09:57 -0700

    initial project setup

---
+++ src/main/java/com/fgonzalez/prueba_ac/PruebaAcApplication.java
@@ -0,0 +1,13 @@
+package com.fgonzalez.prueba_ac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaAcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaAcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fgonzalez/prueba_ac/PruebaAcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fgonzalez.prueba_ac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaAcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
